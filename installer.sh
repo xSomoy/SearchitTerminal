e3d2bf75a362a0632f9068bd1e7d69fefe71796e
xCommunicado <xCommunicado@EOSVM>
2019-06-23 10:08:19 +0600

    shodan filter removed error fixed

--- installer.sh
+++ installer.sh
@@ -521,12 +521,29 @@ function createSearchit() {
     function shodan() {
         dbCheck
         query=\${tmpquery//\ /+}
-        query=\${query/-sdn+}
-        query=\${query/-shodan+}
-        query=\${query/-sdn+}
-        query=\${query/-shodan+}
-        \$browser www.shodan.io/search?query=\$query
+      if [ "\$opParam" == '-exploit' ] || [ "\$opParam" == '-exp' ]
+              then {
+                query=\${query/-exp+}
+                query=\${query/-exploit+}
+                query=\${query/-sdn+}
+                query=\${query/-shodan+}
+                \$browser https://exploits.shodan.io/?q=\$query
+        }
+      elif [ "\$opParam" == '-help' ] || [ "\$opParam" == '-h' ] || [ "\$opParam" == '-?' ]
+          then {
+            echo " Baidu Search Filters:
+                    For Images     -exploit       -exp
+                "
+            exit
+      }
+        else {
+          query=\${query/-sdn+}
+          query=\${query/-shodan+}
+          \$browser www.shodan.io/search?query=\$query
           exit
+        }
+      fi
+
     }
 
     #   Startpage Search Function
