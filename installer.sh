fedd34a2c0273f610dde05ba0807256c37e5141c
xCommunicado <xCommunicado@EOSVM>
2019-07-06 11:55:20 +0600

    working on searx filters

--- installer.sh
+++ installer.sh
@@ -511,9 +511,82 @@ function createSearchit() {
     function searx() {
         dbCheck
         query=\${tmpquery//\ /+}
+        if [ "\$opParam" == '-images' ]  || [ "\$opParam" == '-img' ]
+            then {
+              query=\${query/-img+}
+              query=\${query/-images+}
+              option="images"
+            }
+        elif [ "\$opParam" == '-videos' ] || [ "\$opParam" == '-vid' ]
+            then {
+              query=\${query/-vid+}
+              query=\${query/-videos+}
+              option="videos"
+        }
+      elif [ "\$opParam" == '-news' ] || [ "\$opParam" == '-nws' ]
+            then {
+              query=\${query/-nws+}
+              query=\${query/-news+}
+              option="news"
+        }
+      elif [ "\$opParam" == '-files' ] || [ "\$opParam" == '-fls' ]
+            then {
+              query=\${query/-fls+}
+              query=\${query/-files+}
+              option="files"
+        }
+      elif [ "\$opParam" == '-it' ]
+            then {
+              query=\${query/-it+}
+              option="it"
+        }
+      elif [ "\$opParam" == '-maps' ] || [ "\$opParam" == '-map' ]
+              then {
+                query=\${query/-map+}
+                query=\${query/-maps+}
+                option="map"
+        }
+      elif [ "\$opParam" == '-music' ] || [ "\$opParam" == '-msc' ]
+              then {
+                query=\${query/-msc+}
+                query=\${query/-music+}
+                option="music"
+        }
+      elif [ "\$opParam" == '-science' ] || [ "\$opParam" == '-sci' ]
+              then {
+                query=\${query/-sci+}
+                query=\${query/-science+}
+                option="science"
+        }
+      elif [ "\$opParam" == '-social' ] || [ "\$opParam" == '-scl' ]
+              then {
+                query=\${query/-scl+}
+                query=\${query/-social+}
+                option="social+media"
+        }
+
+      elif [ "\$opParam" == '-help' ] || [ "\$opParam" == '-h' ] || [ "\$opParam" == '-?' ]
+          then {
+            echo " Searx Search Filters:
+                    For Images        -image       -img
+                    For Videos        -Videos      -vid
+                    For News          -news        -nws
+                    For Files         -files       -fls
+                    For It            -it
+                    For Maps          -maps        -map
+                    For Music         -music       -msc
+                    For Science       -science     -sci
+                    For Social Media  -social      -scl
+            "
+            exit
+      }
+        else {
+              option="general"
+        }
+      fi
         query=\${query/-srx+}
         query=\${query/-searx+}
-        \$browser searx.me/?q=\$query
+        \$browser searx.me/?q=\$query\&categories=\$option
     }
 
     #   Shodan Search Function
