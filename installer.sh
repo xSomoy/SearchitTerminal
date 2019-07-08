13b2acfc4a70280504fff048fe0dd79ceb827bb4
xCommunicado <xCommunicado@EOSVM>
2019-07-08 18:15:59 +0600

    added reddit filters

--- installer.sh
+++ installer.sh
@@ -810,9 +810,33 @@ function createSearchit() {
     function reddit() {
         dbCheck
         query=\${tmpquery//\ /+}
+        if [ "\$opParam" == '-post' ] || [ "\$opParam" == '-pst' ]
+                then {
+                  query=\${query/-pst+}
+                  query=\${query/-post+}
+                  option="link"
+            }
+        elif [ "\$opParam" == '-user' ] || [ "\$opParam" == '-usr' ]
+                then {
+                  query=\${query/-usr+}
+                  query=\${query/-user+}
+                  option="sr%2Cuser"
+          }
+        elif [ "\$opParam" == '-help' ] || [ "\$opParam" == '-h' ] || [ "\$opParam" == '-?' ]
+              then {
+                echo " Reddit Search Filters:
+                        For Posts                  -post      -pst
+                        For Communities and users  -user      -usr
+                     "
+                exit
+          }
+        else {
+              option=""
+            }
+        fi
         query=\${query/-rddt+}
         query=\${query/-reddit+}
-        \$browser www.reddit.com/search?q=\$query
+        \$browser www.reddit.com/search?q=\$query\&type=\$option
     }
 
     #   LinkedIn Search Function
