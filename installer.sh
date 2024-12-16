c842b10ba5525d48f14f55c9d6d07d32b3ae46a5
TENET <xSomoy@protonmail.com>
2024-12-16 21:55:52 +0600

    fixes #412

--- installer.sh
+++ installer.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 #############################################################
-#                                                           #
-#     Searchit                                              #
-#     Search The Hell Out Of Internet..                     #
-#                                                           #
-#     Twitter:    www.twitter.com/Tweet2Searchit            #
-#     Facebook:   www.facebook.com/SearchitOfficial         #
-#     Github:     www.github.com/BlackPearlTech             #
-#     Email:      mailtoSearchit@gmail.com                  #
-#     License:    GNU General Public License v3.0           #
-#                                                           #
-#     Author:     TENET [ Mushphyqur Rahman ]                               #
-#     Twitter:    www.twitter.com/xSomoy                    #
-#                                                           #
-#     Copyright © 2024 Mushphyqur Rahman                     #
-#                                                           #
+#                                                                                                                      #
+#     Searchit                                                                                                    #
+#     Search The Hell Out Of Internet..                                                             #
+#                                                                                                                       #
+#     Twitter:    www.twitter.com/Tweet2Searchit                                               #
+#     Facebook:   www.facebook.com/SearchitOfficial                                      #
+#     Github:     www.github.com/BlackPearlTech                                             #
+#     Email:      mailtoSearchit@gmail.com                                                       #
+#     License:    GNU General Public License v3.0                                          #
+#                                                                                                                      #
+#     Author:     TENET [ Mushphyqur Rahman ]                                             #
+#     Twitter:    www.twitter.com/xSomoy                                                         #
+#                                                                                                                      #
+#     Copyright © 2024 Mushphyqur Rahman                                                 #
+#                                                                                                                     #
 #############################################################
 
 version=$(cat coreModules/version.dat)
