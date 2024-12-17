fa33c3025373399ec9e2e8fe5985006f8f80c984
TENET <xSomoy@protonmail.com>
2024-12-17 13:22:59 +0600

    fixed #428

--- coreModules/update.sh
+++ coreModules/update.sh
@@ -2,7 +2,7 @@
 
 function update(){
     cd ~
-    wget -q https://raw.githubusercontent.com/BlackPearlTech/SearchitTerminal/master/resource/version |
+    wget -q https://raw.githubusercontent.com/xSomoy/SearchitTerminal/refs/heads/master/resource/version |
     echo "Checking For Update..."
     if [ -f ~/version ]
     then {
