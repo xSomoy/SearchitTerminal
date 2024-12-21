44ffca3d53f926a04a1746320f684f461d899d73
TENET <xSomoy@protonmail.com>
2024-12-21 20:58:57 +0600

    working 70%

--- wrapper/searchitWrapper.sh
+++ wrapper/searchitWrapper.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
-flags="$2"                              # Catching first flag
+flag="$2"                              # Catching first flag
 main_command="$@"           # whole command
 
-echo "$flags"
+echo "$flag"
 echo "$main_command"
 
-if [ "$flags" == '-'a ] || [ "$flags" == '--about' ] || [ "$flags" == '-h' ] || [ "$flags" == '--help' ] || [ "$flags" == '-?' ] || [ "$flags" == '-un' ] || [ "$flags" == '-'-uninstall ] || [ "$flags" == '-' ] ||
-                then {
-                    connectionTest
-                    drugs
-                }
-            else {
-                connectionTest
-                defaultSearch
+# Declare an array of system flags
+system_flags=("--about" "-a" "--help" "-h" "-?" "--uninstall" "-un" "--update" "-u" "--version" "-v" "--config" "-cfg" "--release" "-rn" "--readme" "-rdm")
+i=0
+echo ${system_flags[i]}
+while [ $i -lt 17 ]  
+do 
+    if [ "$flag" == "${system_flags[i]}" ]
+            then {
+                echo "$flag"
+                $main_command
+                break
             }
-            fi
+    fi
+((i++))
+done
 
-#nohup $parameter  > /dev/null 2>&1 &
+# nohup parameter  > /dev/null 2>&1 &
