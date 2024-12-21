62c577c0781b7ae4c70f9da09582f39927821bbf
TENET <xSomoy@protonmail.com>
2024-12-21 21:06:25 +0600

    wrapper developing done

--- wrapper/searchitWrapper.sh
+++ wrapper/searchitWrapper.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-flag="$2"                              # Catching first flag
+flag="$2"                              # Catching flag
 main_command="$@"           # whole command
-
-echo "$flag"
-echo "$main_command"
 
 # Declare an array of system flags
 system_flags=("--about" "-a" "--help" "-h" "-?" "--uninstall" "-un" "--update" "-u" "--version" "-v" "--config" "-cfg" "--release" "-rn" "--readme" "-rdm")
-i=0
-echo ${system_flags[i]}
+
+# looping through all system flags
+i=0             #indexing variable
 while [ $i -lt 17 ]  
 do 
-    if [ "$flag" == "${system_flags[i]}" ]
+# Checking if the flag is system flag
+    if [ "$flag" == "${system_flags[i]}" ]  
             then {
-                echo "$flag"
-                $main_command
-                break
+                $main_command # Passing command without any change
+                break   # exiting the loop if match found
             }
     fi
 ((i++))
 done
+
+# Executing command in backgroud
 
 nohup $main_command  > /dev/null 2>&1 &
